a11a6baaac844277a86b726569abf4a5242639cb
西帝 <sidi@users.noreply.github.com>
2018-11-30 20:28:27 +0300

    typo

--- data/chroot/brostash-inside-chroot.sh
+++ data/chroot/brostash-inside-chroot.sh
@@ -2,7 +2,7 @@
 
 set -e
 set -x
-# Add logstash key
+# Add elastic repo key
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
 echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" > /etc/apt/sources.list.d/elastic-6.x.list
 apt-get update
